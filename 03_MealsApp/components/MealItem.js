3067e7a5e7f7c6f4f208b52bd3a0aa692278f0e4
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-11-20 16:27:44 +0700

    03_MealsApp - Finish with Stack Navigation Screen, but the last material we want to add drawer navigation to be parent of our Stack Navigator

--- 03_MealsApp/components/MealItem.js
+++ 03_MealsApp/components/MealItem.js
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native"
 import {
   Image,
   Platform,
@@ -6,30 +7,41 @@ import {
   Text,
   View,
 } from "react-native"
+import MealDetails from "./MealDetails"
 
 export default function MealFunction({
+  id,
   title,
   imageUrl,
   duration,
   complexity,
   affordability,
 }) {
+  const navigation = useNavigation()
+
+  function selectMealItemHandler() {
+    navigation.navigate("MealDetail", {
+      mealId: id,
+    })
+  }
+
   return (
     <View style={styles.mealItem}>
       <Pressable
         android_ripple={{ color: "#ccc" }}
         style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+        onPress={selectMealItemHandler}
       >
         <View style={styles.innerContainer}>
           <View>
             <Image source={{ uri: imageUrl }} style={styles.image}></Image>
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
         </View>
       </Pressable>
     </View>
@@ -67,15 +79,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     margin: 18,
-  },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-    justifyContent: "center",
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
   },
 })
--- 03_MealsApp/screens/MealsOverviewScreen.js
+++ 03_MealsApp/screens/MealsOverviewScreen.js
@@ -1,19 +1,32 @@
+import { useEffect, useLayoutEffect } from "react"
 import { FlatList, StyleSheet, Text, View } from "react-native"
 
-import { MEALS } from "../data/dummy-data"
+import { MEALS, CATEGORIES } from "../data/dummy-data"
 import MealItem from "../components/MealItem"
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0
   })
 
+  // useLayoutEffect
+  useEffect(() => {
+    const categotyTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title
+
+    navigation.setOptions({
+      title: categotyTitle,
+    })
+  }, [catId, navigation])
+
   const renderMealItem = (itemData) => {
     const item = itemData.item
 
     const mealItemProps = {
+      id: item.id,
       title: item.title,
       imageUrl: item.imageUrl,
       affordability: item.affordability,
